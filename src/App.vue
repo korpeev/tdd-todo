c5cec10d05c17d63624449a87b7cfd60602f5da2
Aslan <korpeevaslan@gmail.com>
2023-03-07 23:58:43 +0500

    add todo form

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,21 @@
-<script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
-</script>
-
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
+  <div class="h-screen w-screen bg-sky-100 flex justify-center items-center">
+    <div class="shadow-md max-w-[400px] w-full p-5 bg-white">
+      <form>
+        <h2>Type your todo</h2>
+        <div class="flex gap-x-5">
+          <base-input v-model:value="todoInput" />
+          <button class="bg-teal-500 p-1 px-5 text-white rounded font-bold">
+            Add
+          </button>
+        </div>
+      </form>
+    </div>
   </div>
-  <HelloWorld msg="Vite + Vue" />
 </template>
 
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
-</style>
+<script setup lang="ts">
+import { ref } from "vue";
+import BaseInput from "shared/ui/base-input/index.vue";
+const todoInput = ref("");
+</script>
---
+++ src/shared/ui/base-input/index.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <input v-model="model" type="text" class="border rounded p-2" />
+  </div>
+</template>
+
+<script setup lang="ts">
+import { computed } from "vue";
+
+interface Props {
+  value: string;
+}
+
+interface EmitType {
+  (event: "update:value", payload: string): void;
+}
+const props = defineProps<Props>();
+const emit = defineEmits<EmitType>();
+const model = computed({
+  get() {
+    return props.value;
+  },
+  set(value: string) {
+    emit("update:value", value);
+  },
+});
+</script>
